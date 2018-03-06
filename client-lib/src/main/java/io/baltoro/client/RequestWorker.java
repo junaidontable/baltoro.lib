4408376f9777a4834abc503c410388cc00953204
junaid syed <junaid.syed@gmail.com>
2018-03-06 14:53:34 -0800

    response workerpool

--- client-lib/src/main/java/io/baltoro/client/RequestWorker.java
+++ client-lib/src/main/java/io/baltoro/client/RequestWorker.java
@@ -85,7 +85,7 @@ public class RequestWorker extends Thread
 						
 						if(byteBuffer == null)
 						{
-							System.out.println("REQUEST worker thread no work to do , continue .......... "+this+",  --- "+count+",,,"+WorkerPool.info());
+							System.out.println("REQUEST thread no work to do  "+this+",  --- "+count+",,,"+WorkerPool.info());
 							continue;
 						}
 					} 
