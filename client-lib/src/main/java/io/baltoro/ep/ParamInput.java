d91583780690bdc9d50cb96fa7b65c6e6cc9aa27
junaid syed <junaid.syed@gmail.com>
2018-10-21 11:05:51 -0700

    working on webfiles html

--- client-lib/src/main/java/io/baltoro/ep/ParamInput.java
+++ client-lib/src/main/java/io/baltoro/ep/ParamInput.java
@@ -3,6 +3,8 @@ package io.baltoro.ep;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.base.Optional;
+
 @FunctionalInterface
 public interface ParamInput
 {
@@ -14,7 +16,28 @@ public interface ParamInput
 	
 	default ParamInput add(String name, String value)
 	{
+		if(value == null)
+		{
+			value = "";
+		}
 		epData.add(name, value);
+		return this;
+	}
+	
+	default ParamInput add(String name, Optional<String> value)
+	{
+		String _value = null;
+		if(value == null || value.get() == null)
+		{
+			_value = "";
+		}
+		else
+		{
+			_value = value.get();
+		}
+		
+		
+		epData.add(name, _value);
 		return this;
 	}
 	
@@ -26,6 +49,7 @@ public interface ParamInput
 	
 	default EPData getEPData()
 	{
+		
 		get(this);
 		List<Object[]> list = epData.list;
 		EPData data = new EPData();
